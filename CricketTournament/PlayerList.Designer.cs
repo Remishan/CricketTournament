c24a9059934fe4d5a0d0e0e2727e04dcc11a4676
Remi shan <cherukattilremi@gmail.com>
2021-05-21 14:57:58 +0530

    Add project files.

---
+++ CricketTournament/PlayerList.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CricketTournament
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lsbPlayers = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(28, 108);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(127, 32);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(28, 148);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(127, 32);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.Linen;
+            this.txtName.Location = new System.Drawing.Point(28, 72);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(127, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // lsbPlayers
+            // 
+            this.lsbPlayers.BackColor = System.Drawing.Color.Linen;
+            this.lsbPlayers.FormattingEnabled = true;
+            this.lsbPlayers.Location = new System.Drawing.Point(161, 72);
+            this.lsbPlayers.Name = "lsbPlayers";
+            this.lsbPlayers.Size = new System.Drawing.Size(159, 108);
+            this.lsbPlayers.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(25, 56);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(113, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Players List";
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(332, 212);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lsbPlayers);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Registration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "List";
+            this.Load += new System.EventHandler(this.Registration_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ListBox lsbPlayers;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CricketTournament/PlayerList.cs
@@ -0,0 +1,119 @@
+﻿using System;       
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CricketTournament
+{
+    public partial class Registration : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Server=DESKTOP-C0MLP28\SQLEXPRESS;Database=CricketTeamManagement;Trusted_Connection=true;");
+        SqlCommand command;
+        SqlDataReader reader;
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Enter a name");
+            }
+            else
+            {
+                connection.Open();
+                command = connection.CreateCommand();
+                command.CommandText = "TeamSave";
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("Name", txtName.Text);
+                command.ExecuteNonQuery();
+                lsbPlayers.Items.Clear();
+                ReadValues(1);
+                connection.Close();
+                txtName.Clear();
+                MessageBox.Show("Sucessfully Added");
+            }
+          
+        }
+
+        public void ReadValues(int id)
+        {
+            connection = new SqlConnection(@"Server=DESKTOP-C0MLP28\SQLEXPRESS;Database=CricketTeamManagement;Trusted_Connection=true;");
+            connection.Open();
+            command = connection.CreateCommand();
+            command.CommandText = "TeamList";
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string teamId = reader["Id"].ToString();
+                string teamName= reader["Name"].ToString();
+                lsbPlayers.Items.Add(id+"."+teamName);
+                id++;
+            }
+            connection.Close();
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lsbPlayers.SelectedIndex==-1)
+            {
+                MessageBox.Show( "Please select an item");
+                
+                return;
+            }
+            else
+            {
+               for (int i = 0; i <= lsbPlayers.Items.Count ; i++)
+                {
+                    string[] listPlayers = lsbPlayers.SelectedItems[i].ToString().Split('.');
+                   connection = new SqlConnection(@"Server=DESKTOP-C0MLP28\SQLEXPRESS;Database=CricketTeamManagement;Trusted_Connection=true;");
+                    connection.Open();
+                    command = connection.CreateCommand();
+                    command.CommandText = "TeamList";
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string teamId = reader["Id"].ToString();
+                        string teamName = reader["Name"].ToString();
+                      //  connection.Close();
+                        if (listPlayers[1] ==teamName)
+                        {
+                             connection = new SqlConnection(@"Server=DESKTOP-C0MLP28\SQLEXPRESS;Database=CricketTeamManagement;Trusted_Connection=true;");
+                            connection.Open();
+
+                            SqlCommand command = connection.CreateCommand();
+                            command.CommandText = "TeamDelete";
+                            command.CommandType = CommandType.StoredProcedure;
+                            command.Parameters.AddWithValue("Id", teamId);
+                            int result = command.ExecuteNonQuery();
+                            if (result > 0)
+                            {
+                                lsbPlayers.Items.Remove(listPlayers[1]);
+                                lsbPlayers.Items.Clear();
+                                ReadValues(1);
+                                MessageBox.Show("deleted successfully");
+                               
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+            connection.Close();
+        }
+
+        private void Registration_Load(object sender, EventArgs e)
+        {
+            ReadValues(1);
+        }
+    }
+}
